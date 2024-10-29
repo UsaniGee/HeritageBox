fbe9d69e9815f60ea454083c0075beb2e839af47
UsaniGee <usani3121@gmail.com>
2024-10-29 15:10:43 +0100

    Resolved conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import Root from './components/Root.jsx';
 import Home from './pages/Home.jsx';
@@ -12,7 +12,7 @@ import ContactUs from './pages/ContactUs.jsx';
 
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Root />,
